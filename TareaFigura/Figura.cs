61b05f2581681f7aa008c488b7eff8f7a995cd01
lagar <lagar@WILMERPC>
2024-07-21 19:46:37 -0400

    Agregar archivos de proyecto.

---
+++ TareaFigura/Figura.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaFigura
+{
+    abstract class Figura
+    {
+        public abstract double CalcularArea();
+
+
+    }
+    class Rectangulo : Figura 
+    {
+        private double largo;
+        private double ancho;
+        public Rectangulo(double largo, double ancho)
+        {
+            this.largo = largo; 
+            this.ancho = ancho;
+        }
+        public override double CalcularArea()
+        {
+            return largo* ancho;
+        }
+    }
+    class Circulo : Figura
+    {
+        private double radio;
+        public Circulo(double radio)
+        {
+            this.radio = radio;
+        }
+        public override double CalcularArea()
+        {
+            return Math.PI * radio* radio;
+        }
+       
+    }
+    class Triangulo : Figura
+    {
+        private double baseTriangulo;
+        private double alturaTriangulo;
+
+        public Triangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.alturaTriangulo = alturaTriangulo;
+
+
+        }
+        public override double CalcularArea()
+        {
+            return baseTriangulo * alturaTriangulo / 2;
+        }
+
+
+
+    }
+
+}
---
+++ TareaFigura/Program.cs
@@ -0,0 +1,13 @@
+﻿using TareaFigura;
+
+List<Figura> figuras = new List<Figura>();
+
+figuras.Add(new Rectangulo(8, 2));
+figuras.Add(new Circulo(6));
+figuras.Add(new Triangulo(9, 12));
+
+// Calcular y mostrar el área de cada figura
+foreach (Figura figura in figuras)
+{
+    Console.WriteLine($"Área de la figura: {figura.CalcularArea()}");
+}
